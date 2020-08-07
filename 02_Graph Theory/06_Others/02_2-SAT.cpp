78f52be08cad0ebd7c33b76fb04fa5fbf3b36981
WUST-Mengqinyu <badcw123@gmail.com>
2020-08-07 18:52:59 +0800

    Fri Aug  7 18:52:59 CST 2020

--- 02_Graph Theory/06_Others/02_2-SAT.cpp
+++ 02_Graph Theory/06_Others/02_2-SAT.cpp
@@ -10,61 +10,65 @@
 
 const int maxn = 2e6 + 10;
 
-int n, m, a, va, b, vb;
-int low[maxn], dfn[maxn], color[maxn], cnt, scc_cnt;
-bool instack[maxn];
+namespace twosat {
+    int n;
+    int low[maxn], dfn[maxn], color[maxn], cnt, scc_cnt;
+    bool instack[maxn];
 
-vector<int> g[maxn];
-stack<int> st;
-
-void Tarjan(int u)
-{
-    low[u] = dfn[u] = ++cnt;
-    st.push(u);
-    instack[u] = true;
-    for(const auto &v : g[u])
-    {
-        if(!dfn[v]) Tarjan(v), low[u] = min(low[u], low[v]);
-        else if(instack[v]) low[u] = min(low[u], dfn[v]);
+    vector<int> g[maxn];
+    stack<int> st;
+    
+    void init(int _n) {
+        n = _n;
+        cnt = scc_cnt = 0;
+        for (int i = 0; i <= n; ++i) {
+            dfn[i] = 0;
+            g[i].clear();
+        }
     }
-    if(low[u] == dfn[u])
-    {
-        ++scc_cnt;
-        do {
-            color[u] = scc_cnt;
-            u = st.top(); st.pop();
-            instack[u] = false;
-        } while(low[u] != dfn[u]);
+
+    void Tarjan(int u) {
+        low[u] = dfn[u] = ++cnt;
+        st.push(u);
+        instack[u] = true;
+        for (const auto &v : g[u]) {
+            if (!dfn[v]) Tarjan(v), low[u] = min(low[u], low[v]);
+            else if (instack[v]) low[u] = min(low[u], dfn[v]);
+        }
+        if (low[u] == dfn[u]) {
+            ++scc_cnt;
+            do {
+                color[u] = scc_cnt;
+                u = st.top();
+                st.pop();
+                instack[u] = false;
+            } while (low[u] != dfn[u]);
+        }
     }
-}
 
-inline void add(int a, int b) { g[a].push_back(b); }
+    inline void add(int a, int b) { g[a].push_back(b); }
 
-inline void AND(int a, int b, int c)
-{
-    if(c == 1) add(a, a + n), add(b, b + n);
-    else add(a + n, b), add(b + n, a);
-}
+    inline void AND(int a, int b, int c) {
+        if (c == 1) add(a, a + n), add(b, b + n);
+        else add(a, b + n), add(b, a + n);
+    }
 
-inline void OR(int a, int b, int c)
-{
-    if(c == 0) add(a + n, a), add(b + n, b);
-    else add(a + n, b), add(b + n, a);
-}
+    inline void OR(int a, int b, int c) {
+        if (c == 0) add(a + n, a), add(b + n, b);
+        else add(a + n, b), add(b + n, a);
+    }
 
-inline void XOR(int a, int b, int c)
-{
-    if(c == 0) add(a, b), add(a + n, b + n), add(b, a), add(b + n, a + n);
-    else add(a, b + n), add(a + n, b), add(b, a + n), add(b + n, a);
-}
+    inline void XOR(int a, int b, int c) {
+        if (c == 0) add(a, b), add(a + n, b + n), add(b, a), add(b + n, a + n);
+        else add(a, b + n), add(a + n, b), add(b, a + n), add(b + n, a);
+    }
 
-bool TWO_SAT()
-{
-    input();
-    for(int i = 1; i <= (n << 1); i ++) if(!dfn[i]) Tarjan(i);
-    for(int i = 1; i <= n; i ++)
-        if(color[i] == color[i + n]) return false;
-    for(int i = 1; i <= n; i ++)
-        printf("%d ", color[i] > color[i + n]);
-    return true;
+    bool TWO_SAT() {
+        for (int i = 1; i <= (n << 1); i++) if (!dfn[i]) Tarjan(i);
+        for (int i = 1; i <= n; i++)
+            if (color[i] == color[i + n]) return false;
+        for (int i = 1; i <= n; i++)
+            printf("%d ", color[i] > color[i + n]);
+        return true;
+    }
 }

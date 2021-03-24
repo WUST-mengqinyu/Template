fab7c3177f64195dccebf5689deddedc69b5d9bc
Verly <Verly0206@gmail.com>
2021-03-24 22:14:50 +0800

    fix gauss

--- 01_Math/03_线代/02_高斯消元.cpp
+++ 01_Math/03_线代/02_高斯消元.cpp
@@ -4,11 +4,13 @@ double b[maxn], c[maxn][maxn];
 
 void guass() {
     for (int i = 1; i <= n; i++) {
-        for (int j = i; j <= n; j++)
-            if (fabs(c[j][i]) > eps) {
-                for (int k = 1; k <= n; k++) swap(c[i][k], c[j][k]);
-                swap(b[i], b[j]);
-            }
+        int idx = i;
+        for(int j = i + 1; j <= n; j ++) if(fabs(c[j][i]) > fabs(c[idx][i])) idx = j;
+        if(idx != i)
+        { 
+            for(int j = i; j <= n; j ++) swap(c[i][j], c[idx][j]);
+            swap(b[i], b[idx]);
+        }
         for (int j = 1; j <= n; j++) {
             if (i == j) continue;
             double rate = c[j][i] / c[i][i];
